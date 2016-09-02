05345cb1c2ed3dc56fac038eab0a4b1ada46fb2e
Jason Lee <Jason Lee>
2016-09-02 17:55:08 -0400

    Player and City classes, not working

--- Civilization.java
+++ Civilization.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class Civilization {
     static boolean playing = true;
 
+    public class Player {
+        private String civilization;
+        private String name;
+        private City[] cities;
+        private int numAttacks;
+        private double gold;
+        private double resources;
+        private int happiness;
+        private int military;
+        private int technology;
+
+        public Player(String civilization) {
+
+        }
+    }
+
+    public class City {
+        private String name;
+
+        public City (String name) {
+            this.name = name;
+        }
+    }
     // checks if the player is still playing, quit means quit
     public static boolean stillPlaying(String input) {
         boolean answer;
