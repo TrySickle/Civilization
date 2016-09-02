04b0f853587311aa21f87f0805db9b944eb77470
Jason Lee <Jason Lee>
2016-09-02 16:13:44 -0400

    Player input and quitting

--- Civilization.java
+++ Civilization.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Civilization {
     static boolean playing = true;
 
+    // checks if the player is still playing, quit means quit
+    public static boolean stillPlaying(String input) {
+        boolean answer;
+        if (input.equalsIgnoreCase("quit"))
+            answer = false;
+        else
+            answer = true;
+        return answer;
+    }
+
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String command;
         
         while(playing) {
             //Your game code here
 
-            //Use this to break out of the game loop. 
+            //Use this to break out of the game loop.
             //Feel free to move it around.
-            playing = false;
+
+            System.out.print("Enter a command: ");
+            command = keyboard.next();
+            playing = stillPlaying(command);
         }
     }
 }
