7b82830060b9fd4f7e6085e23c9ff7f11b11e105
Jason Lee <Jason Lee>
2016-09-02 21:46:56 -0400

    No classes, civ/name set

--- Civilization.java
+++ Civilization.java
@@ -2,53 +2,95 @@ import java.util.Scanner;
 
 public class Civilization {
     static boolean playing = true;
+    static String input, civilization, name;
+    static String[] cities;
+    static int attacks, happiness, military, technology;
+    static double gold, resources;
 
-    public class Player {
-        private String civilization;
-        private String name;
-        private City[] cities;
-        private int numAttacks;
-        private double gold;
-        private double resources;
-        private int happiness;
-        private int military;
-        private int technology;
+    static final String[] VALID_CIVS =
+        {"American", "Zulu", "English", "Chinese"};
 
-        public Player(String civilization) {
+    static final String[] NAMES =
+        {"George Washington", "Shaka", "Queen Elizabeth I", "Wu Zetian"};
 
+    public static void setCivilization(String aCivilization) {
+        if (!isValidCivilization(aCivilization)) {
+            System.out.println(aCivilization + " is not a valid civilization.");
+            System.exit(0);
+        }
+        civilization = aCivilization;
+    }
+
+    public static boolean isValidCivilization(String civilization) {
+        boolean isValid = false;
+
+        for (String s : VALID_CIVS) {
+            if (s.equalsIgnoreCase(civilization)) {
+                isValid = true;
+            }
+        }
+
+        return isValid;
+    }
+
+    public static void setName(String aCivilization) {
+        for (int i = 0; i < VALID_CIVS.length; i++) {
+            if (VALID_CIVS[i].equalsIgnoreCase(aCivilization))
+                name = NAMES[i];
         }
     }
 
-    public class City {
-        private String name;
-
-        public City (String name) {
-            this.name = name;
-        }
-    }
     // checks if the player is still playing, quit means quit
     public static boolean stillPlaying(String input) {
         boolean answer;
+
         if (input.equalsIgnoreCase("quit"))
             answer = false;
         else
             answer = true;
+
         return answer;
+    }
+
+    public static void initialize() {
+        cities = new String[5];
+        attacks = 0;
+        gold = 20.5;
+        resources = 30.0;
+        happiness = 10;
+        military = 0;
+        technology = 0;
+
+    }
+
+    public static void listCommands() {
+        System.out.println("Settle a City, Demolish a City, Build Militia, Research Technology, Attack Enemy City, End Turn");
+    }
+
+    public static void update() {
+        //will update the player at the beginning of the turn
+        //2 decimal places
+    }
+
+    public static void test() {
+        System.out.println(name);
     }
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        String command;
-        
+
+        System.out.print("What civilization would you like to lead?: ");
+        input = keyboard.next();
+        setCivilization(input);
+        setName(input);
+        initialize();
+
         while(playing) {
-            //Your game code here
-
-            //Use this to break out of the game loop.
-            //Feel free to move it around.
-
-            System.out.print("Enter a command: ");
-            command = keyboard.next();
-            playing = stillPlaying(command);
+            System.out.print("(? for list of commands) Enter a command: ");
+            input = keyboard.next();
+            //checkInput(input);
+            playing = stillPlaying(input);
+            test();
         }
     }
 }
