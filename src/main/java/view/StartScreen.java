ad74232dcb67bb08971c9f86321ed89de51cb0a1
Jason Lee <Jason Lee>
2016-11-14 23:32:04 -0500

    Start screen looks pretty

--- src/main/java/view/StartScreen.java
+++ src/main/java/view/StartScreen.java
@@ -2,16 +2,22 @@ package view;
 
 import model.Civilization;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Button;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.paint.Color;
 
 /**
  * Created by Tian-Yo Yang on 11/11/2016.
@@ -26,6 +32,7 @@ import javafx.collections.ObservableList;
 public class StartScreen extends StackPane {
 
     private BackgroundImage background;
+    private Text selectText;
     private ListView<CivEnum> civListView;
     private Button startButton;
     /**
@@ -33,11 +40,16 @@ public class StartScreen extends StackPane {
     * image and display a list of civilizations and a start button
     */
     public StartScreen() {
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.BOTTOM_CENTER);
         Image bImage = new Image(
             "File:./src/main/java/view/civ_background.png");
         background = new BackgroundImage(bImage, BackgroundRepeat.NO_REPEAT,
             BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
             new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, false, true));
+        selectText = new Text("Select a Civilization to Begin");
+        selectText.setFont(new Font(16));
+        selectText.setFill(Color.RED);
         ObservableList<CivEnum> civs = FXCollections.observableArrayList(CivEnum.ANCIENT_EGYPT,
             CivEnum.QIN_DYNASTY, CivEnum.ROMAN_EMPIRE);
         civListView = new ListView<CivEnum>(civs);
@@ -47,9 +59,10 @@ public class StartScreen extends StackPane {
         civListView.setMinHeight(96);
         civListView.setMaxHeight(96);
         startButton = new Button("START");
+        vbox.getChildren().addAll(selectText, civListView, startButton);
         this.setBackground(new Background(background));
-        this.getChildren().addAll(civListView, startButton);
-
+        this.getChildren().addAll(vbox);
+        this.setMargin(vbox, new Insets(0, 0, 80, 0));
     }
     /**
     * gets the start button
